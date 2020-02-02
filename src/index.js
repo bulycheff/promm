40d0924ef355d119a79724c33d5ea7dc5c0d9f61
Sergey Bulychev <bulycheff@ya.ru>
2020-02-03 02:08:29 +0500

    два ряда квадратов рисуется с помощью промисов ...

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ import * as dat from 'dat.gui'
 var container, stats, controls;
 var camera, scene, renderer, rgbeLoader;
 let head, bridge, laserhead;
-let myReq;
 
 ///////////////////////
 
@@ -113,34 +112,105 @@ function init() {
             const moveHeadGUI = {
                 "move head": () => {
                     SetHeadPosition(.63, 1.77);
-                    console.log('Clicked ...')
                 },
                 "set to null coord": () => {
                     SetHeadPosition(0, 0);
-                    console.log('Clicked ...')
                 },
                 "draw line 1": () => {
-                    DrawLine(-.55, 1.6);
-                    console.log('Draw ...')
+                    DrawLine(.5, -.5);
                 },
                 "draw line 2": () => {
-                    DrawLine(-.1, .5);
-
-                    console.log('Draw ...')
+                    DrawLine(-.5, -.5);
                 },
                 "draw line 3": () => {
-                    DrawLine(.22, -.3);
-
-                    console.log('Draw ...')
+                    DrawLine(-.5, .5);
                 },
                 "draw square": () => {
-                    DrawLine(-.55, .5);
-                    DrawLine(-.55, -.5);
-                    DrawLine(0, -.5);
-                    DrawLine(0, .5);
-                    DrawLine(-.55, .5);
 
-                    console.log('Draw ...')
+                    let xpos = -.65;
+                    let strtx = xpos;
+                    let strtz = -.95;
+                    let sidel = .11;
+                    
+                    DrawSquare(strtx, strtz, sidel)
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx = xpos;
+                            strtz += (sidel + .02);
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+                        .then(() => {
+                            strtx += (sidel + .02)
+                            return DrawSquare(strtx, strtz, sidel)
+                        })
+
                 },
 
             }
@@ -151,7 +221,7 @@ function init() {
             fldr.add(moveHeadGUI, 'draw line 1');
             fldr.add(moveHeadGUI, 'draw line 2');
             fldr.add(moveHeadGUI, 'draw line 3');
-            fldr.add(moveHeadGUI, 'draw line 4');
+            fldr.add(moveHeadGUI, 'draw square');
 
             fldr = datGui.addFolder('LASERHEAD UP / DOWN');
             fldr.open();
@@ -159,161 +229,197 @@ function init() {
             const LaserHeadGUI = {
                 "laserhead down": () => {
                     HeadDown();
-                    console.log('Clicked ...')
                 },
                 "laserhead up": () => {
                     HeadUp();
-                    console.log('Clicked ...')
                 },
             }
 
             fldr.add(LaserHeadGUI, 'laserhead up');
             fldr.add(LaserHeadGUI, 'laserhead down');
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////
-            //установить голову в нужную позицию
+            // РАБОЧЕЕ ПОЛЕ (x, z): (-0.7, -1.1) ... (0.7, 1.9)
+
+            // переместить голову в заданную позицию (подготовка к резке)
 
             const SetHeadPosition = (x, z) => {
 
-                let deltaX = .01;
-                let deltaZ = .01;
+                return new Promise((resolve) => {
 
-                const MoveHead = () => {
+                    let deltaX = .01;
+                    let deltaZ = .01;
 
-                    if (Math.round((bridge.position.z - z) * 100) / 100 < 0) {
+                    const MoveHead = () => {
 
-                        bridge.position.z += deltaZ;
+                        if (Math.round((bridge.position.z - z) * 100) / 100 < 0) {
 
-                    } else if (Math.round((bridge.position.z - z) * 100) / 100 > 0) {
+                            bridge.position.z += deltaZ;
 
-                        bridge.position.z -= deltaZ;
+                        } else if (Math.round((bridge.position.z - z) * 100) / 100 > 0) {
+
+                            bridge.position.z -= deltaZ;
+
+                        }
+
+                        bridge.position.z = (Math.round(bridge.position.z * 100) / 100);
+
+                        if (Math.round((head.position.x - x) * 100) / 100 < 0) {
+
+                            head.position.x += deltaX;
+
+                        } else if (Math.round((head.position.x - x) * 100) / 100 > 0) {
+
+                            head.position.x -= deltaX;
+
+                        }
+
+                        head.position.x = Math.round(head.position.x * 100) / 100;
+
+                        if ((Math.abs(bridge.position.z - z).toFixed(2) >= deltaZ) || (Math.abs(head.position.x - x).toFixed(2) >= deltaX)) {
+
+                            requestAnimationFrame(MoveHead);
+
+                        } else {
+
+                            head.position.x = x;
+                            bridge.position.z = z;
+
+                            resolve();
+
+                        }
 
                     }
 
-                    bridge.position.z = (Math.round(bridge.position.z * 100) / 100);
+                    MoveHead();
 
-                    if (Math.round((head.position.x - x) * 100) / 100 < 0) {
-
-                        head.position.x += deltaX;
-
-                    } else if (Math.round((head.position.x - x) * 100) / 100 > 0) {
-
-                        head.position.x -= deltaX;
-
-                    }
-
-                    head.position.x = Math.round(head.position.x * 100) / 100;
-
-                    if ((Math.abs(bridge.position.z - z).toFixed(2) >= deltaZ) || (Math.abs(head.position.x - x).toFixed(2) >= deltaX)) {
-
-                        myReq = requestAnimationFrame(MoveHead);
-                        console.info('myReq MoveHead #: ', myReq);
-
-                    }
-
-                }
-
-                MoveHead();
+                })
 
             }
+
+            // перемещает линию по прямой (во время резки)
 
             const DrawLine = (x, z) => {
 
-                let pathLength, pathLengthX, pathLengthZ;
-                let pathVelocity;
-                let deltaX, deltaZ, delta;
+                return new Promise((resolve) => {
 
-                pathVelocity = Math.sqrt(2 * .01 ** 2);
-                console.info('pathVelocity: ', pathVelocity)
+                    let pathLength, pathLengthX, pathLengthZ;
+                    let pathVelocity;
+                    let deltaX, deltaZ, delta;
 
-                pathLength = Math.sqrt((head.position.x - x) ** 2 + (bridge.position.z - z) ** 2);
-                console.info('pathLength: ', pathLength);
+                    delta = 5 * 0.00141421356;
 
-                pathLengthX = x - head.position.x;
-                console.info('pathLengthX: ', pathLengthX);
+                    pathLength = Math.sqrt((head.position.x - x) ** 2 + (bridge.position.z - z) ** 2);
+                    pathLengthX = x - head.position.x;
+                    pathLengthZ = z - bridge.position.z;
 
-                pathLengthZ = z - bridge.position.z;
-                console.info('pathLengthZ: ', pathLengthZ);
+                    deltaX = Math.sign(pathLengthX) * delta * Math.abs(Math.cos(Math.atan(pathLengthZ / pathLengthX)));
+                    deltaZ = Math.sign(pathLengthZ) * delta * Math.abs(Math.sin(Math.atan(pathLengthZ / pathLengthX)));
 
-                deltaX = pathLengthX * pathVelocity;
-                console.info('deltaX: ', deltaX)
+                    const DrawLineStep = () => {
 
-                deltaZ = pathLengthZ * pathVelocity;
-                console.info('deltaZ: ', deltaZ)
+                        head.position.x += deltaX;
+                        bridge.position.z += deltaZ;
 
-                delta = Math.sqrt(deltaX ** 2 + deltaZ ** 2);
-                console.info('delta: ', delta)
+                        if (((Math.abs(head.position.x - x) > Math.abs(deltaX)) && (deltaX != 0)) || ((Math.abs(bridge.position.z - z) > Math.abs(deltaZ)) && (deltaZ != 0))) {
 
-                const DrawLineStep = () => {
+                            requestAnimationFrame(DrawLineStep)
 
-                    head.position.x += deltaX;
-                    console.info('head.position.x: ', head.position.x)
+                        } else {
 
-                    bridge.position.z += deltaZ;
-                    console.info('bridge.position.z: ', bridge.position.z)
+                            head.position.x = x;
+                            bridge.position.z = z;
 
-                    if (Math.sqrt((head.position.x - x) ** 2 + (bridge.position.z - z) ** 2) > pathVelocity) {
+                            resolve();
 
-                        myReq = requestAnimationFrame(DrawLineStep)
-                        console.info('myReq DrawLineStep #: ', myReq);
-
+                        }
                     }
-                }
 
-                DrawLineStep();
+                    DrawLineStep();
+
+                })
+
 
             }
+
+            // устанавливает лазерную голову по высоте
 
 
             const SetLaserHeadPos = (y) => {
 
-                let deltaY = .005;
-                let rnDeltaY = 1 / deltaY;
+                return new Promise((resolve) => {
 
-                const MoveLaserHead = () => {
+                    let deltaY = .005;
+                    let rnDeltaY = 1 / deltaY;
 
-                    if (Math.round((laserhead.position.y - y) * rnDeltaY) / rnDeltaY < 0) {
+                    const MoveLaserHead = () => {
 
-                        console.info(laserhead.position.y)
+                        if (Math.round((laserhead.position.y - y) * rnDeltaY) / rnDeltaY < 0) {
 
-                        laserhead.position.y += deltaY;
+                            laserhead.position.y += deltaY;
 
-                        console.info(laserhead.position.y)
+                        } else if (Math.round((laserhead.position.y - y) * rnDeltaY) / rnDeltaY > 0) {
 
-                    } else if (Math.round((laserhead.position.y - y) * rnDeltaY) / rnDeltaY > 0) {
+                            laserhead.position.y -= deltaY;
 
-                        console.info(laserhead.position.y)
+                        }
 
-                        laserhead.position.y -= deltaY;
+                        laserhead.position.y = Math.round(laserhead.position.y * rnDeltaY) / rnDeltaY;
 
-                        console.info(laserhead.position.y)
+                        if (Math.abs(laserhead.position.y - y) >= deltaY) {
+
+                            requestAnimationFrame(MoveLaserHead);
+
+                        } else {
+                            
+                            laserhead.position.y = y;
+
+                            resolve();
+
+                        }
 
                     }
 
-                    laserhead.position.y = Math.round(laserhead.position.y * rnDeltaY) / rnDeltaY;
+                    MoveLaserHead();
 
-                    console.info(laserhead.position.y)
-
-                    if (Math.abs(laserhead.position.y - y) >= deltaY) {
-
-                        requestAnimationFrame(MoveLaserHead);
-
-                    }
-
-                }
-
-                MoveLaserHead();
+                })
 
             }
 
             const HeadDown = () => SetLaserHeadPos(-.065);
             const HeadUp = () => SetLaserHeadPos(0);
 
+            const DrawSquare = (xStart, zStart, sideLength) => {
 
+                return new Promise((resolve) => {
+
+                    SetHeadPosition(xStart, zStart)
+                        .then(() => {
+                            return HeadDown();
+                        })
+                        .then(() => {
+                            return DrawLine(xStart + sideLength, zStart)
+                        })
+                        .then(() => {
+                            return DrawLine(xStart + sideLength, zStart + sideLength)
+                        })
+                        .then(() => {
+                            return DrawLine(xStart, zStart + sideLength)
+                        })
+                        .then(() => {
+                            return DrawLine(xStart, zStart)
+                        })
+                        .then(() => {
+                            return HeadUp();
+                        })
+                        .then(() => {
+                            resolve();
+                        })
+
+                })
+
+            }
 
         });
-
-
 
     });
 
@@ -348,7 +454,6 @@ function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
@@ -357,11 +462,9 @@ function onWindowResize() {
 
 function animate() {
 
-    myReq = requestAnimationFrame(animate);
-    console.info('myReq animate #: ', myReq);
-
+    requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    // console.log(camera.position)
-
+    controls.update();
     stats.update();
+
 }
